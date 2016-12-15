62f4f3ea69871531fe0474d143138ae91c8f8934
Aravind Murugesan <aravindspring@gmail.com>
2016-12-15 11:47:35 +1000

    Added Factory and refined code

--- seqcommon.js
+++ seqcommon.js
@@ -1,26 +1,67 @@
 _spBodyOnLoadFunctionNames.push("callAngularBootstrap");
 (function () {
     "use strict";
-    var homeapp = angular.module('Seq-SafetyTip-app', []);
-    homeapp.controller("LatestSafetyTipController", LatestSafetyTipController);
-    function LatestSafetyTipController($scope ,$http,$q) {
+    var serviceModule = angular.module('Common-Services', []);
+    serviceModule.factory('sharePointDataService', ['$log', '$q', '$http', '$timeout', function sharepointDataService($log, $q, $http, $timeout) {
+        $log.log("Factory: sharepointDataService");
+        var service = {
+            getItems: getItems
+        };
+        activate();
+        return service;
 
+        var readyFlag = false;
+        var webUrl;
+        var siteCollectionUrl;
+        function activate() {
+            $log.log("sharepointDataService activate");
+            SP.SOD.executeFunc('sp.js', 'SP.ClientContext', function () {
+                $log.log("SP ready");
+                webUrl = SP.PageContextInfo.get_webServerRelativeUrl();
+                siteCollectionUrl = SP.PageContextInfo.get_siteServerRelativeUrl();
+                readyFlag = true;
+            });
+        }
+        function spVariablesReady(){
+            var deferred = $q.defer();
+            checkFlagReady();
+            return deferred.promise;
+            function checkFlagReady() {
+                if (readyFlag) {
+                    $log.log("SP is ready")
+                    deferred.resolve();
+                } else {
+                    $log.log("SP not ready, waiting timeout")
+                    $timeout(checkFlagReady, 100, false);
+                }
+            }
+        }
         function getItems(webRelativeUrl) {
-            var deferred = $q.defer();            
-                $http({
-                    method: "GET",
-                    url: "http://sp2016/sites/seqtest" + "/" + webRelativeUrl,
-                    headers: { 'Accept': 'application/json;odata=verbose' }
-                }).success(function (data) {
-                    //$log.log("Data service - Got Web list items")
-                    deferred.resolve(data.d.results);
-                }).error(function (data, status) {
-                    deferred.reject(data);
-                });      
+            var deferred = $q.defer();
+            spVariablesReady().then(function(){ $http({
+                method: "GET",
+                url: webUrl + "/" + webRelativeUrl,
+                headers: { 'Accept': 'application/json;odata=verbose' }
+            }).success(function (data) {
+                //$log.log("Data service - Got Web list items")
+                deferred.resolve(data.d.results);
+            }).error(function (data, status) {
+                deferred.reject(data);
+            });
+        });
 
             return deferred.promise;
-        }
-        getItems("_api/web/lists/getByTitle('Latest Safety Tip')/items?$select=SeqwaterSafetyTipContent,SeqwaterSafetyTipURL")
+        }       
+
+    }]);
+
+    //Module for SafetTips
+    var safetyTipsApp = angular.module('Seq-SafetyTip-Mod', ['Common-Services']);
+    safetyTipsApp.controller("LatestSafetyTipController", LatestSafetyTipController);
+   // LatestSafetyTipController.$inject=["$scope", "$http", "$q","sharePointDataService"];
+    function LatestSafetyTipController($scope, $http, $q,$log,sharePointDataService) {
+
+        sharePointDataService.getItems("_api/web/lists/getByTitle('Latest Safety Tip')/items?$filter=((SeqwaterSafetyTipFeatured eq 1) and (OData__ModerationStatus eq 0))&$orderby=Modified desc&$top=1&$select=SeqwaterSafetyTipContent,SeqwaterSafetyTipURL")
                    .then(getSafetyTipSuccess, getSafetyTipError);
         function getSafetyTipSuccess(data) {
             $scope.SafetyTip = [];
@@ -29,28 +70,15 @@ _spBodyOnLoadFunctionNames.push("callAngularBootstrap");
             });
         }
         function getSafetyTipError(reason) {
-            console.log(JSON.stringify(reason));
+            $log.log(JSON.stringify(reason));
         }
     }
-    var homeapp1 = angular.module("Seq-favlinks-app", [])
-    homeapp1.controller("FavLinksController", FavLinksController);
-    function FavLinksController($scope, $http, $q) {
-        function getItems(webRelativeUrl) {
-            var deferred = $q.defer();
-            $http({
-                method: "GET",
-                url: "http://sp2016/sites/seqtest" + "/" + webRelativeUrl,
-                headers: { 'Accept': 'application/json;odata=verbose' }
-            }).success(function (data) {
-                //$log.log("Data service - Got Web list items")
-                deferred.resolve(data.d.results);
-            }).error(function (data, status) {
-                deferred.reject(data);
-            });
-
-            return deferred.promise;
-        }
-        getItems("_api/web/lists/getByTitle('tt1')/items?$select=URL")
+    //Module for Favourite Links
+    var favLinksApp = angular.module("Seq-FavLinks-Mod", ['Common-Services'])
+    favLinksApp.controller("FavLinksController", FavLinksController);
+    function FavLinksController($scope, $http, $q, $log, sharePointDataService) {
+   
+        sharePointDataService.getItems("_api/web/lists/getByTitle('tt1')/items?$select=URL")
                    .then(getFavouriteLinksSuccess, getFavouriteLinksError);
         function getFavouriteLinksSuccess(data) {
             $scope.favLinks = [];
@@ -59,7 +87,7 @@ _spBodyOnLoadFunctionNames.push("callAngularBootstrap");
             });
         }
         function getFavouriteLinksError(reason) {
-            console.log(JSON.stringify(reason));
+            $log.log(JSON.stringify(reason));
         }
     }
 
@@ -72,7 +100,7 @@ function callAngularBootstrap() {
     //var angSafetyTipApp = document.getElementById("homecontainer");
     //angular.bootstrap(angSafetyTipApp, ["homepage-angularapp"]);
     var angSafetyTipApp = document.getElementById("Seq-SafetyTip");
-    angular.bootstrap(angSafetyTipApp, ['Seq-SafetyTip-app']);
+    angular.bootstrap(angSafetyTipApp, ['Common-Services', 'Seq-SafetyTip-Mod']);
     var angFavLinksApp = document.getElementById("Seq-favlinks");
-    angular.bootstrap(angFavLinksApp, ['Seq-favlinks-app']);
+    angular.bootstrap(angFavLinksApp, ['Common-Services', 'Seq-FavLinks-Mod']);
 }
